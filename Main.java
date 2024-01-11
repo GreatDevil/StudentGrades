86eeb54aa64b5b9c2674ee0cef93eb90af9eb786
ilya <ilya.botarev@yandex.ru>
2024-01-11 20:57:15 +0300

    add main

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        handler(showMenu());
+    }
+
+    private static void handler(String input) {
+        if(Arrays.stream(Menu.values()).map(Enum::name).toList().contains(input)) {
+            action(Menu.valueOf(input));
+        }
+    }
+
+    private static void action(Menu menu) {
+        System.out.println(menu.answer);
+        System.out.println(menu.action.apply(System.console().readLine()));
+        handler(showMenu());
+    }
+
+    private static String showMenu() {
+        Arrays.stream(Menu.values()).map(r -> r.describe).forEach(System.out::println);
+        return System.console().readLine().toUpperCase();
+    }
+}
