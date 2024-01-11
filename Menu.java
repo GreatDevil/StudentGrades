1d3e3eedf05e680f2124267885452564e3432c7e
ilya <ilya.botarev@yandex.ru>
2024-01-11 20:57:02 +0300

    create menu

---
+++ Menu.java
@@ -0,0 +1,19 @@
+import java.util.function.Function;
+
+public enum Menu {
+    A("a. Добавьте нового ученика", "введите имя", Journal::addStudent),
+    B("b. Удалите ученика", "введите имя", Journal::removeStudent),
+    C("c. Обновите оценку ученика", "введите имя ученика и оценку через пробел", Journal::updateGrade),
+    D("d. Просмотр оценок всех учащихся", "нажмите ввод для просмотра", Journal::showAllGrades),
+    E("e. Просмотр оценок конкретного учащегося", "введите имя", Journal::showStudentGrades);
+
+    public final String describe;
+    public final String answer;
+    public final Function<String, String> action;
+    Menu(String describe, String answer, Function<String, String> action) {
+        this.describe = describe;
+        this.answer = answer;
+        this.action = action;
+    }
+}
+
