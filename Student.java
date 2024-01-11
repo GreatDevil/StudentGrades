d7a5e816ee1407a1ced1fdd0b6bf0175add812e9
ilya <ilya.botarev@yandex.ru>
2024-01-11 20:56:07 +0300

    create student

---
+++ Student.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class Student {
+    private String name;
+    private List<Byte> grades;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Byte> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(List<Byte> grades) {
+        this.grades = grades;
+    }
+}
